b6536248bf0a87a7555c1fad7f1de3dabe951678
Kiril Yakymchuk <stbestich@gmail.com>
2025-03-24 19:03:17 +0200

    chore: base app configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger as PinoLogger } from 'nestjs-pino';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+    cors: true,
+  });
+
+  const config = app.get<ConfigService>(ConfigService);
+  const logger = new Logger(bootstrap.name);
+
+  const PORT = config.getOrThrow<number>('HTTP_PORT');
+  const HOST = config.getOrThrow<string>('HTTP_HOST');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  app.useLogger(app.get(PinoLogger));
+  app.use(helmet());
+  app.enableCors();
+
+  await app.listen(PORT, HOST, () => {
+    logger.log(`Service is running on http://${HOST}:${PORT}`);
+  });
 }
 bootstrap();
