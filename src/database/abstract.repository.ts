8141b3ebac51758d0c416d60148d70ee9d6957a3
Kiril Yakymchuk <stbestich@gmail.com>
2025-03-26 17:35:41 +0200

    chore: abstract repository

--- src/database/abstract.repository.ts
+++ src/database/abstract.repository.ts
@@ -1,7 +1,17 @@
 import { Model, ModelCtor } from 'sequelize-typescript';
-import { Logger } from '@nestjs/common';
+import {
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { IRepository } from './repository.interface';
-import { CreationAttributes, WhereOptions } from 'sequelize';
+import {
+  BaseError,
+  CreationAttributes,
+  ModelAttributes,
+  WhereOptions,
+} from 'sequelize';
+import { v7 as uuidv7 } from 'uuid';
 
 export abstract class AbstractRepository<T extends Model>
   implements IRepository<T>
@@ -10,27 +20,79 @@ export abstract class AbstractRepository<T extends Model>
 
   protected constructor(protected readonly model: ModelCtor<T>) {}
 
-  public create(attributes: CreationAttributes<T>): Promise<T> {
-    throw new Error('Method not implemented.');
+  public async create(attributes: CreationAttributes<T>): Promise<T> {
+    return this.catchable<T>(() =>
+      this.model.create({
+        id: uuidv7(),
+        ...attributes,
+      }),
+    );
   }
 
-  public findByPk(primaryKey: number): Promise<T> {
-    throw new Error('Method not implemented.');
+  public async findByPk(primaryKey: string | number): Promise<T> {
+    const entity = await this.catchable<T | null>(() =>
+      this.model.findByPk(primaryKey),
+    );
+    if (!entity) {
+      throw new NotFoundException(
+        `Entity ${this.model.name} not found by id ${primaryKey}`,
+      );
+    }
+    return entity;
   }
 
-  public findOne(options: WhereOptions<T>): Promise<T> {
-    throw new Error('Method not implemented.');
+  public async findOne(options: WhereOptions<T>): Promise<T> {
+    const entity = await this.catchable<T | null>(() =>
+      this.model.findOne({ where: options }),
+    );
+    if (!entity) {
+      throw new NotFoundException(
+        `Entity ${this.model.name} not found by options ${JSON.stringify(options)}`,
+      );
+    }
+
+    return entity;
   }
 
-  public findAll(options?: WhereOptions<T>): Promise<T[]> {
-    throw new Error('Method not implemented.');
+  public async findAll(options?: WhereOptions<T>): Promise<T[]> {
+    const entities = await this.catchable<T[]>(() =>
+      this.model.findAll({ where: options }),
+    );
+    if (!entities.length) {
+      throw new NotFoundException(
+        `Any entity ${this.model.name} not found by options ${JSON.stringify(options)}`,
+      );
+    }
+    return entities;
   }
 
-  public update(options?: Partial<CreationAttributes<T>>): Promise<T> {
-    throw new Error('Method not implemented.');
+  public async update(
+    primaryKey: string | number,
+    options: Partial<ModelAttributes<T>>,
+  ): Promise<T> {
+    const entity = await this.findByPk(primaryKey);
+    return this.catchable<T>(() => entity.set(options).save());
   }
 
-  public delete(primaryKey: number): Promise<void> {
-    throw new Error('Method not implemented.');
+  public async delete(primaryKey: string | number): Promise<void> {
+    const entity = await this.findByPk(primaryKey);
+    return this.catchable<void>(() => entity.destroy());
+  }
+
+  private async catchable<R>(callback: () => Promise<R>): Promise<R> {
+    try {
+      return await callback();
+    } catch (e) {
+      this.logger.error(e instanceof Error ? e.message : e);
+
+      if (e instanceof BaseError) {
+        throw new InternalServerErrorException(`Sequelize error`, {
+          cause: e.message,
+          description: `Error while performing database operation on model ${this.model.name}`,
+        });
+      }
+
+      throw new InternalServerErrorException();
+    }
   }
 }
--- src/database/repository.interface.ts
+++ src/database/repository.interface.ts
@@ -1,11 +1,14 @@
 import { Model } from 'sequelize-typescript';
-import { CreationAttributes, WhereOptions } from 'sequelize';
+import { CreationAttributes, ModelAttributes, WhereOptions } from 'sequelize';
 
 export interface IRepository<T extends Model> {
   create(attributes: CreationAttributes<T>): Promise<T>;
-  findByPk(primaryKey: number): Promise<T>;
+  findByPk(primaryKey: string | number): Promise<T>;
   findOne(options: WhereOptions<T>): Promise<T>;
   findAll(options?: WhereOptions<T>): Promise<T[]>;
-  update(options?: Partial<CreationAttributes<T>>): Promise<T>;
-  delete(primaryKey: number): Promise<void>;
+  update(
+    primaryKey: string | number,
+    options?: Partial<ModelAttributes<T>>,
+  ): Promise<T>;
+  delete(primaryKey: string | number): Promise<void>;
 }
