3ce03af450c17b193618d7853410ee02c2df94ff
Kiril Yakymchuk <stbestich@gmail.com>
2025-03-26 16:34:20 +0200

    chore: added db dialect check on module level

--- src/database/database.factory.ts
+++ src/database/database.factory.ts
@@ -3,6 +3,7 @@ import { PostgresModule } from './postgres.module';
 import { SqliteModule } from './sqlite.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
+import { ConnectionRefusedError } from 'sequelize';
 
 type DatabaseModuleName = 'postgres' | 'sqlite';
 
@@ -31,7 +32,13 @@ export class DatabaseFactoryModule {
       sqlite: SqliteModule,
     };
 
-    return modules[this.config.getOrThrow<DatabaseModuleName>('DB_DIALECT')];
+    const dbDialect = this.config.getOrThrow<DatabaseModuleName>('DB_DIALECT');
+    if (dbDialect !== 'postgres' && dbDialect !== 'sqlite') {
+      throw new ConnectionRefusedError(
+        new Error('Database dialect must be postgres or sqlite.'),
+      );
+    }
+    return modules[dbDialect];
   }
 
   public static forRoot(): DynamicModule {
